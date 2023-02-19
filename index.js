77b95a7bdd1ecb260aa5f935f4658d562ca83711
ZewdieMc <zewdiehabtie26@gmail.com>
2023-02-19 21:45:28 +0300

    Add entry point js file

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-import { DateTime } from "luxon";
+import { navList, navAdd, navContact } from './modules/navList.js';
+import UI from './modules/awesomeBooksUI.js';
+import singlePageDisplay from './modules/single-page-display.js';
+import renderTime from './modules/DateTimeDisplay.js';
+
+window.onload = () => {
+  UI.renderBooks();
+  document.querySelector('.form-add').addEventListener('click', (event) => {
+    event.preventDefault();
+    UI.addBook();
+  });
+
+  renderTime();
+  navList.addEventListener('click', (event) => {
+    singlePageDisplay(event);
+  });
+
+  navAdd.addEventListener('click', (event) => {
+    singlePageDisplay(event);
+  });
+
+  navContact.addEventListener('click', (event) => {
+    singlePageDisplay(event);
+  });
+};
