1b046e7e69fc37eda6cb38106abf52886f5e040e
ZewdieMc <zewdiehabtie26@gmail.com>
2023-02-19 22:19:26 +0300

    Update Luxon usage

--- modules/DateTimeDisplay.js
+++ modules/DateTimeDisplay.js
@@ -1,6 +1,6 @@
-import { DateTime } from './luxon.js';
-
 export default () => {
+  // eslint-disable-next-line no-undef
+  const { DateTime } = luxon;
   const timeDisplay = document.querySelector('.date-div');
   const dt = DateTime.local().toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY);
   timeDisplay.textContent = dt;
